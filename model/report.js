9a6ff216e441424933c5df4349a31051028a2c52
ankitFrance <ankitfrance2020@gmail.com>
2024-02-26 15:15:41 +0100

    form   1 completed

--- model/report.js
+++ model/report.js
@@ -10,7 +10,84 @@ const reportSchema = new mongoose.Schema({
         },
         Role: {
             type: String
-        }
+        }, 
+        Qualification: {
+            type: String
+        },
+        Date_of_inspection: {
+            type: Date
+        }, 
+        Tools: {
+            type: String
+        }, 
+        Methods: {
+            type: String
+        }, 
+        Purpose_of_condition_report : {
+            type: String
+        },
+        Purpose_of_condition_report : {
+            type: String
+        }, 
+        Name_of_client: {
+            type: String 
+        }, 
+        Reliability:{
+            type: String
+        }, 
+        Height:{
+            type: String
+        }, 
+        Examination_center:{
+            type: String
+        }, 
+        Platform:{
+            type: String
+        }, 
+        Behind_glass:{
+            type: String
+        }, 
+        Quantity:{
+            type: String
+        }, 
+        Quality:{
+            type: String
+        }, 
+        Nature:{
+            type: String
+        }, 
+        Ruler:{
+            type: String, 
+            default: 'no'
+        }, 
+        Microscope:{
+            type: String, 
+            default: 'no'
+        }, 
+        Camera:{
+            type: String,
+            default: 'no'
+        }, 
+        Gloves:{
+            type: String,
+            default: 'no'
+        }, 
+        Clothing:{
+            type: String,
+            default: 'no'
+        }, 
+        Person_present_name:{
+            type: String
+        }, 
+        Person_present_role:{
+            type: String
+        }, 
+        Duration_of_assessment:{
+            type: String
+        }, 
+        Inaccessibility:{
+            type: String
+        }, 
     }
       
    
--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -34,12 +34,36 @@ router.post('/feedback', uploadMiddleware.fields([{ name: 'ImageFile'}, { name: 
 
       Name: formData.name,
       Role: formData.role,
-      
+      Qualification: formData.qualification,
+      Date_of_inspection: formData.date_of_inspection,
+      Tools: formData.tools,
+      Methods: formData.methods, 
+      Purpose_of_condition_report: formData.purpose_of_condition_report,
+      Name_of_client: formData.name_of_client,
+      Reliability: formData.reliability, 
+      Height: formData.height,
+      Examination_center: formData.examination_center,
+      Platform: formData.platform,
+      Behind_glass: formData.behind_glass, 
+      Quantity: formData.quantity, 
+      Quality: formData.quality, 
+      Nature: formData.nature, 
+      Ruler: formData.ruler, 
+      Microscope: formData.microscope, 
+      Camera: formData.camera, 
+      Gloves: formData.gloves, 
+      Clothing: formData.clothing, 
+      Person_present_name: formData.person_present_name,
+      Person_present_role: formData.person_present_role,
+      Duration_of_assessment: formData.duration_of_assessment,
+      Inaccessibility: formData.inaccessibility
+     
+
     }
    
    
   });
-  console.log(formData.name)
+  console.log(formData)
   await ReportForm.save();
 
 
