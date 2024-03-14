e58a767658dd9d85afee438859cc6e3a92e080df
ankitFrance <ankitfrance2020@gmail.com>
2024-03-14 19:40:20 +0100

    modal

--- public/logic.js
+++ public/logic.js
@@ -34,6 +34,9 @@ document.addEventListener('DOMContentLoaded', function () {
         var progressBarWidth = progresStep * 20;
         document.querySelector('.progress-bar').style.width = progressBarWidth + '%';
     }
+
+
+ 
 });
 
 //******************************************************************************** 
@@ -46,12 +49,123 @@ function displayUploadMessage(containerID) {
 
 //******************************************************************************** 
 
+function viewSummary() {
+
+  
+  const name = document.getElementById('name').value;              // Fs means = for summary
+  const role = document.getElementById('role').value;
+  const institution = document.getElementById('institution').value;
+  const doi = document.getElementById('doi').value;
+  const tools = document.getElementById('tools').value;
+  const methods = document.getElementById('methods').value;
+  const purpose_of_condition_report = document.getElementById('purpose_of_condition_report').value;
+  const name_of_client = document.getElementById('name_of_client').value;
+  const height = document.getElementById('height').value;
+  const examination_center = document.getElementById('examination_center').value;
+  const platform = document.getElementById('platform').value;
+  const behind_glass = document.getElementById('behind_glass').value;
+  const quantity = document.getElementById('quantity').value;
+  const quality = document.getElementById('quality').value;
+  const nature = document.getElementById('nature').value;
+  const ruler = document.getElementById('ruler').value;
+  const microscope = document.getElementById('microscope').value;
+  const camera = document.getElementById('camera').value;
+  const gloves = document.getElementById('gloves').value;
+  const clothing = document.getElementById('clothing').value;
+  const person_present_name = document.getElementById('person_present_name').value;
+  const person_present_role = document.getElementById('person_present_role').value;
+  const duration_of_assessment = document.getElementById('duration_of_assessment').value;
+  const inaccessibility = document.getElementById('inaccessibility').value;
+  
+
+  var summaryText = `
+  <div style="position: relative;">
+  <h5 style="color: white; background-color: green; padding: 10px; margin-bottom: 0;"> 
+    REPORTING INFORMATION 
+    <i class="fas fa-pencil-alt" style="float: right;"></i>
+  </h5> 
+  <hr style="border-color: darkblue; margin-top: 0;">
+  </div>
+  <p><strong> Name:</strong> ${name} </p>
+  <p><strong> Role:</strong> ${role} </p>
+  <p><strong> Institution:</strong> ${institution} </p>
+  <p><strong> Date of Inspection:</strong> ${doi} </p>
+  <p><strong> Tools:</strong> ${tools} </p>
+  <p><strong> Methods:</strong> ${methods} </p>
+  <p><strong> Purpose of Condition Report:</strong> ${purpose_of_condition_report} </p>
+  <p><strong> Name of Client :</strong> ${name_of_client} </p>
+  <p><strong> Height:</strong> ${height} </p>
+  <p><strong> Examination center:</strong> ${examination_center} </p>
+  <p><strong> Platform:</strong> ${platform} </p>
+  <p><strong> Behind glass:</strong> ${behind_glass} </p>
+  <p><strong> Quantity:</strong> ${quantity} </p>
+  <p><strong> Quality:</strong> ${quality} </p>
+  <p><strong> Nature:</strong> ${nature} </p>
+  <p><strong> Ruler ?:</strong> ${ruler} </p>
+  <p><strong> Microscope ?:</strong> ${microscope} </p>
+  <p><strong> Camera ?:</strong> ${camera} </p>
+  <p><strong> Gloves ?:</strong> ${gloves} </p>
+  <p><strong> Clothing ?:</strong> ${clothing} </p>
+  <p><strong> Person Present name:</strong> ${person_present_name} </p>
+  <p><strong> Person Present Role:</strong> ${person_present_role} </p>
+  <p><strong> Duration of assessment:</strong> ${duration_of_assessment} </p>
+  <p><strong> Inaccessibility of part of Asset:</strong> ${inaccessibility} </p>
+
+  <div style="position: relative;">
+  <h5 style="color: white; background-color: green; padding: 10px; margin-bottom: 0;"> 
+  OBJECT IDENTIFICATION
+    <i class="fas fa-pencil-alt" style="float: right;"></i>
+  </h5> 
+  <hr style="border-color: darkblue; margin-top: 0;">
+  </div>
+
+  <div style="position: relative;">
+  <h5 style="color: white; background-color: green; padding: 10px; margin-bottom: 0;"> 
+  OBJECT DESCRIPTION 
+    <i class="fas fa-pencil-alt" style="float: right;"></i>
+  </h5> 
+  <hr style="border-color: darkblue; margin-top: 0;">
+  </div>
+
+  <div style="position: relative;">
+  <h5 style="color: white; background-color: green; padding: 10px; margin-bottom: 0;"> 
+  OBJECT ENVIRONMENT
+    <i class="fas fa-pencil-alt" style="float: right;"></i>
+  </h5> 
+  <hr style="border-color: darkblue; margin-top: 0;">
+  </div>
+
+  <div style="position: relative;">
+  <h5 style="color: white; background-color: green; padding: 10px; margin-bottom: 0;"> 
+  CONDITIONS DESCRIPTION (OBEJCT)
+    <i class="fas fa-pencil-alt" style="float: right;"></i>
+  </h5> 
+  <hr style="border-color: darkblue; margin-top: 0;">
+  </div>
+
+  <div style="position: relative;">
+  <h5 style="color: white; background-color: green; padding: 10px; margin-bottom: 0;"> 
+  DIAGNOSTIC AND RECOMMENDATIONS
+    <i class="fas fa-pencil-alt" style="float: right;"></i>
+  </h5> 
+  <hr style="border-color: darkblue; margin-top: 0;">
+  </div>
+`;
+
+
+  document.getElementById('summaryDiv').innerHTML = summaryText;
+
+  let summaryModal = new bootstrap.Modal(document.getElementById('summaryModal'));
+  summaryModal.show();
+}
+
+
+
+
+function closeModal() {
+  $('#summaryModal').modal('hide');
  
-
-
-
-
-
+}
 
   
   function changeLanguage() {
@@ -187,22 +301,6 @@ changeLanguage();   // Initialize placeholders with default language
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   function record(){
 
     var recognition = new webkitSpeechRecognition();
@@ -218,23 +316,7 @@ changeLanguage();   // Initialize placeholders with default language
 
  
 
-  function viewSummary() {
 
-
-    // Get values from the form
-    var name = document.getElementById('name').value;
-
-    var role = document.getElementById('role').value;
-
-    var summaryText = "<h5> Name: " + name + "</h5>";
-    
-
-    // Display the summary in the designated div
-    document.getElementById('summaryDiv').innerHTML = summaryText;
-
-    let summaryModal = new bootstrap.Modal(document.getElementById('summaryModal'));
-    summaryModal.show();
-}
 
 
 
@@ -248,3 +330,6 @@ function getTodayDate() {
 
 // Set the value of the date input to today's date
 document.getElementById('doi').value = getTodayDate();
+
+
+
